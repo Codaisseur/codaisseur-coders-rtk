49698663f85ba7b2c45bc7545f38f48683c4a1b0
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-05-05 11:18:11 +0200

    homepage post feed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import Homepage from "./pages/Homepage";
 
 export default function App() {
   return (
     <div>
       <Routes>
         {/* more pages to be added here later */}
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </div>
   );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,55 @@
-// src/pages/HomePage.js
-import React from "react";
+// src/pages/Homepage.js
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import axios from "axios";
 
-export default function HomePage() {
-  return <h1>Hello!</h1>;
+const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
+
+export default function Homepage() {
+  const [data, setData] = useState({
+    loading: true,
+    posts: [],
+  });
+
+  async function fetchPosts() {
+    setData({ ...data, loading: true }); //before fetching data, loading is set to true
+
+    const response = await axios.get(`${API_URL}/posts`);
+
+    const posts = response.data.rows;
+
+    setData({
+      loading: false, //once the data is fetched, loading is set to false
+      posts: posts,
+    });
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div style={{ marginLeft: 20 }}>
+      <h2>Posts</h2>
+      {data.posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p className="meta">
+              {moment(post.createdAt).format("DD-MM-YYYY")} &bull;{" "}
+              <span className="tags">
+                {post.tags.map((tag) => {
+                  return (
+                    <React.Fragment key={tag.id}>
+                      <span className="Tag">{tag.tag}</span>{" "}
+                    </React.Fragment>
+                  );
+                })}
+              </span>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
