88b29aa0125450b873f80e91e48ba2deada5587d
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-05-10 12:26:20 +0200

    Add code of Authentication day to app

---
+++ src/pages/Login.js
@@ -0,0 +1,52 @@
+// src/pages/LoginPage.js
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../store/auth/actions";
+import { getAuthLoading } from "../store/auth/selectors";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loading = useSelector(getAuthLoading);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(login(email, password, navigate));
+  }
+
+  return (
+    <div style={{ marginLeft: 30 }}>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            Email:{" "}
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Password:{" "}
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Login</button>
+        </p>
+      </form>
+      {loading && "Loading..."}
+    </div>
+  );
+}
---
+++ src/store/auth/selectors.js
@@ -0,0 +1,2 @@
+export const getUserProfile = (reduxState) => reduxState.auth.me;
+export const getAuthLoading = (reduxState) => reduxState.auth.loading;
